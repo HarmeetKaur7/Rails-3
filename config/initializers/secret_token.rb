87d5c8f286cb1d029c98fa0da3bf2f108f3147cf
HarmeetKaur7 <harmeet.kaur@freshworks.com>
2022-08-26 14:02:02 +0530

    Update all changes till CRUD

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'b8b397628f93feb7196b70dfb7362ab20a94ea5593b830f26ed0c8f20c4507f9a2638fd70a48d6c2772448fae2ecd627a637698788044e2873604c59da07e1c2'
